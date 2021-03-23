e320c57ef80eef9a0ff654213bf75585a83993f5
Parth Kalkar <parthkalkar111@gmail.com>
2021-03-23 12:23:29 +0300

    Linear search
    Linear search algorithm

---
+++ Linear Search.py
@@ -0,0 +1,15 @@
+arr = [4, 2, 8, 9, 3, 7]
+
+x = int(input("Enter number to search: "))
+
+found = False
+
+for i in range(len(arr)):
+ if(arr[i] == x):
+  found = True
+  print("%d found at %dth position"%(x,i))
+  break
+ 
+if(found == False):
+ print("%d is not in list"%x)
+ 
